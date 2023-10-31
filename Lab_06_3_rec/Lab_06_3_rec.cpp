354d307442e9b690230fea21f92dcd6def4947a6
Maksym Cherniienko <h0nza4ok@gmail.com>
2023-10-31 20:37:21 +0200

    Add project files.

---
+++ Lab_06_3_rec/Lab_06_3_rec.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+	a[i] = Low + rand() % (High - Low + 1);
+	if (i < size - 1)
+		Create(a, size, Low, High, i + 1);
+}
+
+void Print(int* a, const int size, int i)
+{
+	cout << setw(4) << a[i];
+	if (i < size - 1)
+		Print(a, size, i + 1);
+}
+
+void Sort(int* a, const int size, int i)
+{ // метод обміну (бульбашки)
+	int k = 0; // показник, чи були обміни
+	for (int j = 0; j < size - i; j++) // номер поточного елемента
+		if (a[j] < a[j + 1]) // якщо порушено порядок
+		{ // - обмін елементів місцями
+			int tmp = a[j];
+			a[j] = a[j + 1];
+			a[j + 1] = tmp;
+			k = 1;
+		}
+	if (k == 0) // якщо обмінів - не було,
+		return; // то припиняємо процес
+	if (i < size - 1) // i - лічильник ітерацій
+		Sort(a, size, i + 1); // перехід до наступної ітерації
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	const int n = 10;
+	int a[n];
+	int Low = 1;
+	int High = 31;
+	Create(a, n, Low, High, 0);
+	cout << "array =        ["; Print(a, n, 0); cout << "  ]" << endl;
+	Sort(a, n, 1);
+	cout << "sorted array = ["; Print(a, n, 0); cout << "  ]";
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_3_rec/Lab_06_3_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int a[n];
+			int Low = 1;
+			int High = 31;
+			Create(a, n, Low, High, 0);
+			Sort(a, n, 1);
+			// чи в порядку спадання
+			for (int i = 0; i < n - 1; i++) {
+				Assert::IsTrue(a[i] >= a[i + 1]);
+
+			}
+		};
+	};
+
+}
